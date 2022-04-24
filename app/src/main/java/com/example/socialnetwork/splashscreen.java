7e52cdacbb95d596509365961ffde46dd280c00f
YuvrajAgarwal-ai <65359524+YuvrajAgarwal-ai@users.noreply.github.com>
2022-04-24 10:08:30 +0530

    new

---
+++ app/src/main/java/com/example/socialnetwork/splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.socialnetwork;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashscreen extends AppCompatActivity {
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashscreen.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+
+}
+
